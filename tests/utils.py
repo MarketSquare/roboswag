bc64340c19ed48fee746f2fdfd563015eca54e0e
Bartłomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-08-15 12:20:48 +0200

    Add first tests (#37)
    * Add first tests
    
    * Fix args type

---
+++ tests/utils.py
@@ -0,0 +1,35 @@
+import contextlib
+import os
+from pathlib import Path
+from typing import List
+
+from click.testing import CliRunner
+
+from roboswag.cli import cli
+
+TEST_DATA_DIR = Path(__file__).parent / "test_data"
+
+
+def run_cli(args: List[str], exit_code: int = 0, temp_work_dir: bool = True):
+    runner = CliRunner()
+    arguments = args if args is not None else []
+    if temp_work_dir:
+        with runner.isolated_filesystem():
+            result = runner.invoke(cli, arguments)
+    else:
+        result = runner.invoke(cli, arguments)
+    if result.exit_code != exit_code:
+        print(result.output)
+        raise AssertionError(f"robotidy exit code: {result.exit_code} does not match expected: {exit_code}")
+    return result
+
+
+@contextlib.contextmanager
+def working_directory(path):
+    """Changes working directory and returns to previous on exit"""
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
