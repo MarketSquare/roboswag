257c5b19fa398a55e1042521699ff67a98aeb1a2
Bartlomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-08-16 13:15:09 +0200

    Validate payload with schema validator

--- roboswag/validate/schema.py
+++ roboswag/validate/schema.py
@@ -8,14 +8,14 @@ from roboswag.validate.core import ValidateBase
 
 
 class ValidateSchema(ValidateBase):
-    def schema(self, response, schema: Union[str, Path, Dict]):
+    def schema(self, to_validate, schema: Union[str, Path, Dict]):
         self.logger.info("Validating schema...")
         if isinstance(schema, (str, Path)):
             with open(schema) as fp:
                 schema = json.load(fp)
         self.logger.debug(f"Schema:\n{json.dumps(schema, indent=4)}")
         try:
-            jsonschema.validate(instance=response.json(), schema=schema)
+            jsonschema.validate(instance=to_validate, schema=schema)
         except jsonschema.exceptions.ValidationError as err:
             raise err
         self.logger.info("Schema is valid.")
