f2818dada8ce2843089c4a08df415a7a67dd05e4
Bartlomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-08-20 20:56:16 +0200

    Fix path to generated spec file

--- tests/example_api/simple_v3/main.py
+++ tests/example_api/simple_v3/main.py
@@ -20,8 +20,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-def generate_openapi():
-    with open("tmp_openapi.json", "w") as fp:
+def generate_openapi(spec_path: Path):
+    with open(spec_path, "w") as fp:
         json.dump(
             get_openapi(
                 title=app.title,
@@ -36,6 +36,6 @@ def generate_openapi():
 
 
 def test_gen_spec_and_library():
-    generate_openapi()
-    generated_spec = Path(__file__).parent / "tmp_openapi.json"
-    run_cli(f"generate -s {generated_spec}".split())
+    spec_path = Path(__file__).parent / "tmp_openapi.json"
+    generate_openapi(spec_path)
+    run_cli(f"generate -s {spec_path}".split())
