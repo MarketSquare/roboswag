9e046451a29c1cd14833e55fa9c466125feef481
Bartlomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-08-20 21:09:50 +0200

    Add tests for yaml OpenAPI file

--- tests/atest/generate/test_generate.py
+++ tests/atest/generate/test_generate.py
@@ -9,3 +9,8 @@ def test_openapi_v2_missing_optionals():
 def test_openapi_v2_missing_one_tag():
     spec = TEST_DATA_DIR / "missing_one_tag_v2" / "spec.json"
     run_cli(f"generate -s {spec}".split())
+
+
+def test_yaml_v3():
+    spec = TEST_DATA_DIR / "yaml_v3" / "spec.yaml"
+    run_cli(f"generate -s {spec}".split())
