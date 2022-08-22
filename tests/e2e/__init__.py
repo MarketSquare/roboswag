4d4446bcd925238d0cd4ab44decaa5e08aa62b1b
Bartlomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-08-22 08:20:53 +0200

    Create full e2e test: generate spec and run robot tests against it

--- tests/e2e/__init__.py
+++ tests/e2e/__init__.py
@@ -1,9 +1,12 @@
 import json
+import subprocess
 from pathlib import Path
 
+import uvicorn
 from fastapi.openapi.utils import get_openapi
 
 from ..utils import run_cli
+from .api_server import Server
 
 
 def remove_path(path: Path):
@@ -37,8 +40,27 @@ def generate_openapi(app, spec_path: Path):
         json.dump(spec, fp)
 
 
+def get_api_server(app):
+    config = uvicorn.Config(app, host="127.0.0.1", port=8000, log_level="error")
+    return Server(config=config)
+
+
+def run_robot(source, output_dir):
+    cmd = f"robot --outputdir {output_dir} {source}".split()
+    result = subprocess.run(cmd)
+    if result.returncode != 0:
+        raise AssertionError("Robot tests failed.")
+
+
 def run_e2e(app, test_name: str):
     results_dir = prepare_test_result_dir(test_name)
     spec_path = results_dir / "openapi.json"
     generate_openapi(app, spec_path)
     run_cli(f"generate -s {spec_path} -o {results_dir}".split())
+
+    robot_test = Path(__file__).parent / test_name / "test.robot"
+    if not robot_test.is_file():
+        return
+    server = get_api_server(app)
+    with server.run_in_thread():
+        run_robot(robot_test, results_dir)
---
+++ tests/e2e/api_server.py
@@ -0,0 +1,22 @@
+import contextlib
+import threading
+import time
+
+import uvicorn
+
+
+class Server(uvicorn.Server):
+    def install_signal_handlers(self):
+        pass
+
+    @contextlib.contextmanager
+    def run_in_thread(self):
+        thread = threading.Thread(target=self.run)
+        thread.start()
+        try:
+            while not self.started:
+                time.sleep(1e-3)
+            yield
+        finally:
+            self.should_exit = True
+            thread.join()
